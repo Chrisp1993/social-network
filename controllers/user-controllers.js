4076647cfdfa40b72909717cc73333777c880b67
Chrisp1993 <chrispollio93@gmail.com>
2022-08-10 17:00:16 -0400

    added the const userController to get all users, or specifically get users by id

---
+++ controllers/user-controllers.js
@@ -0,0 +1,37 @@
+const { User, Thought } = require('../public/models');
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find().populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    // get user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'Error: user not found' });
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
