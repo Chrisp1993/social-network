a866c892b18992908d80276bb4eb50f5c3615ffe
Chrisp1993 <chrispollio93@gmail.com>
2022-08-10 16:48:00 -0400

    added the throught controller and the index.js along with Thought and User.js files

---
+++ controllers/thought-controller.js
@@ -0,0 +1,38 @@
+const { Thought, User } = require('../public/models');
+
+const thoughtController = {
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find()
+            .populate({
+                path: 'reactions',
+                select: '-__v',
+            })
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get thought by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'Error: thought not found' });
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
