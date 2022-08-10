f958ddcc5283402809d900ded82f6e68d800f73d
Chrisp1993 <chrispollio93@gmail.com>
2022-08-10 17:11:09 -0400

    set up the routes for all users, user by id, creating a user, updating a user, deleting a user, adding or deleting a friend

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,31 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
 
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
+// set up GET all, POST at /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
 
-// add user prefix to user routes
-router.use('/users', userRoutes);
-// add thoughts prefix to thought routes
-router.use('/thoughts', thoughtRoutes);
+// set up GET one, PUT, DELETE at /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
+// set up POST, DELETE routes for friends at /api/users/:id/friends/:friendId
+router 
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
 
 module.exports = router;
